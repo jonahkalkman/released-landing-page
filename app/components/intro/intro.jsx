ef509b1bf441aa14b2d6bd5bd22da19209c537f9
Jonah Kalkman <jonahkalkman@MacBook-Pro-van-Jonah.local>
2021-12-22 23:57:13 +0100

    Added notification svg

--- app/components/intro/intro.jsx
+++ app/components/intro/intro.jsx
@@ -7,8 +7,11 @@ export default function Intro() {
   return (
     <div className='c-intro'>
       <Image src="/logo-small.svg" width="60" height="60" objectFit="contain" />
-      <h2>Released, is your release radar</h2>
+      <h2>Released, is your <span className="intro__green">release</span> radar</h2>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin mattis purus sed neque fringilla bibendum. Sed sit amet accumsan augue. Duis augue quam, dapibus at tellus vitae, tincidunt semper.</p>
+      <div className="intro__image">
+        <Image src="/notification.svg" width="300" height="93" objectFit="contain" />
+      </div>
       <style jsx global>
         {globalStyles}
       </style>
--- app/components/intro/styles.js
+++ app/components/intro/styles.js
@@ -5,4 +5,12 @@ export default css.global`
     margin: 111px 0px 0px 0px;
     text-align: center;
   }
+
+  .intro__green {
+    color: #48C993;
+  }
+
+  .intro__image {
+    margin: 92px 0px 83px 0px;
+  }
 `
